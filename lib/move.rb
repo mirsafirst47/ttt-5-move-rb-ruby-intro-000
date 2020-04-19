aa6153c6179572733bd8697834cea3f7ff78e078
Samir Triande <habib.triande@gmail.com>
2020-04-19 16:03:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,9 +11,9 @@ def input_to_index(user_input)
   user_input = ("1 - 9").to_i
 end
 
-def move(board, input_to_index, X)
+def move(board, user_input, "X")
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  board[input_to_index] = ("X")
+  board[user_input] = ("X")
 end
 
 
